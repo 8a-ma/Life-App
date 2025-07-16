680432cfac22cd6a4740861a9809f133159cd13b
8a-ma <e0as0k98x001b@gmail.com>
2025-07-16 16:41:19 -0400

    api create

--- api/main.py
+++ api/main.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from flask import jsonify
 
-def create_app(enviroment):
+def create_app():
     app = Flask(__name__)
     return app
 
 app = create_app()
+
+
+@app.route('/', methods=['GET'])
+def get_data():
+    response = {
+        'data':{
+            'message': 'success'
+        }
+    }
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
