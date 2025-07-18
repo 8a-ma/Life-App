589325d67074275d4ffecbf3a58e8bd15b878d1d
8a-ma <e0as0k98x001b@gmail.com>
2025-07-18 11:48:08 -0400

    Models definition

--- api/models.py
+++ api/models.py
@@ -2,6 +2,74 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+
+class DailyToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.Date, nullable=False)
+
+
+class YearAgenda(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    year = db.Column(db.Integer)
+    title = db.Column(db.Date, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    date = db.Column(db.Date, nullable=False)
+
+
+class AnnualEvents(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    date = db.Column(db.Date, nullable=False)
+
+
+
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+    # archived = db.Column(db.Boolean, nullable=True)
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.String(10), nullable=False)
+    # archived = db.Column(db.Boolean, nullable=True)
+
+
+
+class CourseList(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True,autoincrement=True)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
+
+class Courses(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True,autoincrement=True)
+    # course_list_id = db.Column(db.Integer, ForeignKey('course_list.id'))
+    # notes_id = db.Column(db.Integer, ForeignKey('notes.id'))
+    name = db.Column(db.String(100))
+    status = db.Column(db.String(10))
+    # progress = Column(Integer)
+
+
+
+class WorkList(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    company = db.Column(db.String(50))
+    cargo = db.Column(db.String(50))
+
+class WorkJournal(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    # works_list_id = Column(Integer, ForeignKey('works_list.id'))
+    title = db.Columnu(db.String(100))
+    created_at = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+
