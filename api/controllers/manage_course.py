8e9d02fcebb1617ab5cbc24951e395bb1e346def
8a-ma <e0as0k98x001b@gmail.com>
2025-07-24 15:39:01 -0400

    feat: manage course endpoint

---
+++ api/controllers/manage_course.py
@@ -0,0 +1,131 @@
+from flask import Blueprint, request, jsonify
+from models.models import db, Courses
+
+manage_course = Blueprint('manage_course', __name__, url_prefix='/course')
+
+@manage_course.route('/<int:course_id>', methods=['GET'])
+def get_course_by_id(course_id):
+    # req_json = request.get_json(silent=True)
+    records = Courses.query.filter(
+        db.extract("id", Courses.id) == course_id
+    ).all()
+
+    return jsonify(records), 200
+
+@manage_course.route('/', methods=['GET'])
+def get_course():
+    # req_json = request.get_json(silent=True)
+    records = Courses.query.limit(10).all()
+
+    response = [
+        {
+            'id': course.id,
+            'name': course.name,
+            'description': course.description,
+            'status': course.status,
+            'progress': course.progress
+        }
+        for course in records
+    ]
+
+    return jsonify(response), 200
+
+@manage_course.route('/', methods=['POST'])
+def post_course():
+    req_json = request.get_json(silent=True)
+    name = req_json['name']
+    description = req_json['description']
+    status = req_json['status']
+    progress = int(req_json['progress'])
+
+    new_course = Courses(name=name,
+                         description=description,
+                         status=status,
+                         progress=progress
+                         )
+    
+    db.session.add(new_course)
+    return jsonify(
+        {
+            'data': {
+                'message': 'Event create successfully',
+                'status': True,
+                'id': new_course.id
+            }
+        }, 201
+    )
+
+
+@manage_course.route('/<course_id>', methods=['PATCH'])
+def patch_course(course_id):
+    req_json = request.get_json(silent=True)
+
+    name = req_json['name']
+    description = req_json['description']
+    status = req_json['status']
+    progress = int(req_json['progress'])
+
+    course_entry = Courses.query.get(course_id)
+
+    if course_entry:
+        course_entry.name = name
+        course_entry.description = description
+        course_entry.status = status
+        course_entry.progress = progress
+
+        db.session.commit()
+
+        return jsonify(
+        {
+            'data': {
+                'message': 'Event update successfully',
+                'status': True,
+                'id': course_entry.id
+            }
+        }, 200
+    )
+    else:
+        return jsonify(
+        {
+            'data': {
+                'message': 'Id agenda not found',
+                'status': False,
+                'id': course_entry.id
+            }
+        }, 404
+    )
+
+
+
+@manage_course.route('/', methods=['DELETE'])
+def delete_course():
+    req_json = request.get_json(silent=True)
+
+    id_ = int(req_json['id'])
+    
+    course_deleted = Courses.query.get(id_)
+
+    if course_deleted:
+
+        db.session.delete(course_deleted)
+        db.session.commit()
+
+        return jsonify(
+            {
+                'data': {
+                    'message': 'Event deleted successfully',
+                    'status': True,
+                    'id': id_
+                }
+            }, 200
+        )
+    else:
+        return jsonify(
+        {
+            'data': {
+                'message': 'Id agenda not found',
+                'status': False,
+                'id': id_
+            }
+        }, 404
+    )
--- api/models/models.py
+++ api/models/models.py
@@ -32,7 +32,6 @@ class Notes(db.Model):
     title = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False)
-    # archived = db.Column(db.Boolean, nullable=True)
 
 class Projects(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
@@ -40,35 +39,30 @@ class Projects(db.Model):
     description = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False)
     status = db.Column(db.String(10), nullable=False)
-    # archived = db.Column(db.Boolean, nullable=True)
 
 
-
-class CourseList(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True,autoincrement=True)
-    name = db.Column(db.String)
-    description = db.Column(db.Text)
 
 class Courses(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True,autoincrement=True)
-    course_list_id = db.Column(db.Integer, db.ForeignKey(CourseList.id))
-    notes_id = db.Column(db.Integer, db.ForeignKey(Notes.id))
-    name = db.Column(db.String(100))
-    status = db.Column(db.String(10))
-    # progress = Column(Integer)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=True)
+    status = db.Column(db.String(10), nullable=False)
+    progress = db.Column(db.Integer, nullalble=False)
 
-
-
-class WorkList(db.Model):
+class Courses_Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    company = db.Column(db.String(50))
-    cargo = db.Column(db.String(50))
+    course_id = db.Column(db.Integer, db.ForeignKey(Courses.id))
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+
 
 class WorkJournal(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    works_list_id = db.Column(db.Integer, db.ForeignKey(WorkList.id))
     title = db.Column(db.String(100))
     created_at = db.Column(db.DateTime)
     content = db.Column(db.Text)
+    company = db.Column(db.String(50))
+    cargo = db.Column(db.String(50))
 
 
