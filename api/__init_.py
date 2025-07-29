9e22b6195c0533a658cde0a2a5c2adf939fe8fc1
8a-ma <e0as0k98x001b@gmail.com>
2025-07-29 13:02:41 -0400

    endpoint __init__.py

--- api/__init_.py
+++ api/__init_.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from models.models import db
 from controllers.manage_calendar import manage_calendar
+from controllers.manage_company import companies
+from controllers.manage_course_notes import notes_course
+from controllers.manage_course import manage_course
+from controllers.manage_notes import notes
+from controllers.manage_project import manage_project
+from controllers.manage_todo import manage_to_do
+from controllers.manage_work_journal import work_journal
 
 def create_app(env):
     app = Flask(__name__)
@@ -11,5 +18,12 @@ def create_app(env):
         db.create_all()
 
     app.register_blueprint(manage_calendar)
+    app.register_blueprint(companies)
+    app.register_blueprint(notes_course)
+    app.register_blueprint(manage_course)
+    app.register_blueprint(notes)
+    app.register_blueprint(manage_project)
+    app.register_blueprint(manage_to_do)
+    app.register_blueprint(work_journal)
 
     return app
